47a9d135db0664195adcf015d3b5107976d657dd
Sinan Gül <sinangul@gmail.com>
2016-05-06 06:22:00 +0000

    database structer file

--- sharegifts.sql
+++ sharegifts.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : Homestead
-Source Server Version : 50709
-Source Host           : 172.168.10.11:3306
+Source Server         : sinangul.com
+Source Server Version : 50547
+Source Host           : localhost:3306
 Source Database       : sharegifts
 
 Target Server Type    : MYSQL
-Target Server Version : 50709
+Target Server Version : 50547
 File Encoding         : 65001
 
-Date: 2016-05-04 08:41:11
+Date: 2016-05-06 09:21:43
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -32,14 +32,14 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `email` varchar(150) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
   `fb_id` bigint(20) DEFAULT NULL,
   `register_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unq_username` (`name`) USING BTREE,
-  UNIQUE KEY `unq_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8mb4;
+  UNIQUE KEY `unq_email` (`email`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for users__gifts
@@ -54,10 +54,10 @@ CREATE TABLE `users__gifts` (
   `claim_at` int(11) DEFAULT NULL,
   `expired_at` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `frn_sender_id` (`sender_id`),
-  KEY `frn_receiver_id` (`receiver_id`),
-  KEY `frn_gift_id` (`gift_id`),
-  CONSTRAINT `frn_gift_id` FOREIGN KEY (`gift_id`) REFERENCES `gifts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `frn_receiver_id` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `frn_sender_id` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+  KEY `frn_sender_id` (`sender_id`) USING BTREE,
+  KEY `frn_receiver_id` (`receiver_id`) USING BTREE,
+  KEY `frn_gift_id` (`gift_id`) USING BTREE,
+  CONSTRAINT `users__gifts_ibfk_1` FOREIGN KEY (`gift_id`) REFERENCES `gifts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `users__gifts_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `users__gifts_ibfk_3` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
